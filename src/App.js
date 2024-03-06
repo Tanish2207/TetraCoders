dfc1382f056cde185c52270a99d1f45d37ef2279
Kartikay-0111 <kartikay7905@gmail.com>
2024-03-06 17:16:49 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Navbar from './components/library/Navbar.jsx';
+import Home from './components/library/home.jsx';
+
 import Landing_page from './components/landing_page/landing_page';
 
 import {Link, Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <Landing_page />
+  
   );
 }
 
