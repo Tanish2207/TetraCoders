7846bbae651792bedc51d66575476e3ce795eef3
Kartikay-0111 <kartikay7905@gmail.com>
2024-03-06 23:07:38 +0530

    updated the library page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import './App.css';
 import Navbar from './components/library/Navbar.jsx';
 import Home from './components/library/home.jsx';
-
+import Input2 from './components/library/input2.jsx';
 import Landing_page from './components/landing_page/landing_page';
-
+import Input from './components/library/input.jsx';
 import { Link, Route, Routes } from 'react-router-dom'
 function App() {
   return (
@@ -12,6 +12,8 @@ function App() {
     <>
       <Home />
       <Navbar />
+      {/* <Input /> */}
+      {/* <Input2 /> */}
     </>
    
   );
