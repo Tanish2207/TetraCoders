5f03e740e5297f0fcf7afa4c9e9f82948626cabc
Prathamesh Sankhe <144032313+PMS61@users.noreply.github.com>
2024-03-06 19:23:38 +0530

    calendar Added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@
 import './App.css';
 import Navbar from './components/library/Navbar.jsx';
 import Home from './components/library/home.jsx';
+import Studentregistration from './components/login/registrationpage1.jsx';
+import data from './components/calendar/DataBase/data.jsx';
+import Events from './components/calendar/Events/Events.jsx';
+import { useEffect } from 'react';
 
-import Landing_page from './components/landing_page/landing_page';
 
-import { Link, Route, Routes } from 'react-router-dom'
 function App() {
+  useEffect(()=>{
+    // const DATA = JSON.parse(localStorage.getItem('events'));
+    // if(!DATA){
+    //   localStorage.setItem("events", JSON.stringify(data));
+    // }
+    localStorage.setItem("events", JSON.stringify(data));
+  },[])
+
   return (
-    // <Landing_page />
-    <>
-      <Home />
-      <Navbar />
-    </>
-   
+    <div className="App">
+      {/* <Navbar />
+      <Home /> */}
+      <Events />
+
+    </div>
   );
 }
 
