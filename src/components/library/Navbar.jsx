f8f6fc58cef92b9629c2ffe986725da43a091ef4
Prathamesh Sankhe <144032313+PMS61@users.noreply.github.com>
2024-03-06 23:20:51 +0530

    router

--- src/components/library/Navbar.jsx
+++ src/components/library/Navbar.jsx
@@ -1,11 +1,12 @@
 import 'w3-css/w3.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
  
     return ( 
         <div className='w3-bottom w3-bar w3-aqua'>
             <ul className='w3-cell-row'>
-                <li className='w3-container w3-red w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Interview Experiences</a></li>
+                <li className='w3-container w3-red w3-cell'><Link className='w3-button w3-black w3-bar-item' to="/studentr">Interview Experiences</Link></li>
                 <li className='w3-container w3-red w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Study Materials</a></li>
                 <li className='w3-container w3-red w3-cell'><a className='w3-center w3-black w3-text-white w3-bar-item'>Contribute</a></li>
                 <li className='w3-container w3-red w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Interview questions</a></li>
