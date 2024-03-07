8416b9e1edc4667d52dffd14bee508df9ca47785
Kartikay-0111 <kartikay7905@gmail.com>
2024-03-07 06:57:17 +0530

    add  input pages

--- src/App.js
+++ src/App.js
@@ -15,37 +15,40 @@ import Experience from './components/library/inexpe';
 import ChatApp from './components/chat/chat.jsx';
 import Collegeregistration from './components/login/registrationpage3';
 import Companyregistration from './components/login/registrationpage2';
-import {  Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import { useEffect } from 'react';
-
+import Input from './components/library/input.jsx';
+import Input2 from './components/library/input2.jsx';
 
 
 function App() {
-  useEffect(()=>{
-    
+  useEffect(() => {
+
     localStorage.setItem("events", JSON.stringify(data));
-  },[])
+  }, [])
 
   return (
 
     // <Landing_page />
-    
+
     <div className="App">
       <BrowserRouter>
-      <Routes>
-         <Route path="/companyr" element= {<Companyregistration/>} />
-         <Route path="/colleger" element= {<Collegeregistration/>} />
-         <Route path="/studentr" element= {<Studentregistration/>} />
-         <Route path="/events" element= {<Events/>} />
-         <Route path="/" element= {<Landing_page/>} />
+        <Routes>
+          <Route path="/companyr" element={<Companyregistration />} />
+          <Route path="/colleger" element={<Collegeregistration />} />
+          <Route path="/studentr" element={<Studentregistration />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/" element={<Landing_page />} />
+          <Route path="/input" element={<Input />} />
+          <Route path="/navbar" element={<Navbar />} />
+          <Route path="/chat" element={<ChatApp />} />
+          <Route path="/exp" element={<Experience />} />
+          <Route path="/dashboard" element={<Student_dashboard />} />
+          <Route path="/libhome" element={<Home />} />
+          <Route path="/input2" element={<Input2 />} />
+        </Routes>
+      </BrowserRouter>
 
-         <Route path="/chat" element= {<ChatApp/>} />
-         <Route path="/exp" element= {<Experience/>} />
-         <Route path="/dashboard" element= {<Student_dashboard/>} />
-         <Route path="/libhome" element= {<Home/>} />
-      </Routes>
-     </BrowserRouter>
-      
 
     </div>
 
--- src/components/library/Navbar.jsx
+++ src/components/library/Navbar.jsx
@@ -1,32 +1,45 @@
 import 'w3-css/w3.css';
-import "./Navbar.css";
-const Navbar = () => {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+const Navbar = () => {
+const navigate = useNavigate();
+const [condition, setCondition] = useState(false)
     return (
         <div className="w3-container ">
             <div className='w3-bottom w3-bar w3-black'>
                 <ul className='w3-cell-row'>
                     <li className='w3-container w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Interview Experiences</a></li>
                     <li className='w3-container w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Study Materials</a></li>
-                    <li className='w3-container w3-cell'><a href='/input' type='button'className='w3-circle w3-button w3-black w3-bar-item'>Contribute</a></li>
+                    <li className='w3-container w3-cell'><a type='button' onClick={() => { document.getElementById('id1').style.display = "block" }} className='w3-circle w3-button w3-black w3-bar-item'>Contribute</a></li>
                     <li className='w3-container w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Interview questions</a></li>
                     <li className='w3-container w3-cell'><a className='w3-button w3-black w3-bar-item' href="/">Home</a></li>
                 </ul>
             </div>
-            {/* <div id="id01" className="select">
-                    <div className="panel">
-                        <h2 className='w3-blue' >Choose A category  </h2><span onclick="document.getElementById('id01').style.display='none'"
-                            className="w3-button w3-display-topright">&times;</span>
-                        <div>
-                            <select name="category" id="category">
-                                <option value="default" selected disabled>Choose from the following</option>
-                                <option value="experience">Sharing experience</option>
-                                <option value="questions">Interview Questions</option>
-                                <option value="materials">StudyMaterials</option>
-                            </select>
-                        </div>
-                    </div>
-            </div> */}
+            <div className='w3-middle'>
+            <div style={{
+                display: "none",
+                position: "fixed",
+                bottom: "8vh",
+                width: "70%"
+                }} id="id1" className=" w3-animate-zoom w3-blue w3-text-white w3-card-4">
+                <div className="w3-padding w3-block">
+                    <div className='w3-blue w3-xlarge'>Choose A category  <span onClick={() => { document.getElementById('id1').style.display = 'none' }}
+                        className="w3-hover-none w3-hover-blue w3-button w3-display-right">&times;</span></div>
+                    <input onClick={()=>setCondition(false)} value="experience" type="radio" name="category" id="inex" />
+                    <label htmlFor="inex">Interview Experience</label>
+                    <br />
+                    <input onClick={()=>setCondition(false)} value="questions" type="radio" name="category" id="inqu" />
+                    <label htmlFor="inqu">Interview Questions</label>
+                    <br />
+                    <input onClick={()=>setCondition(true)} value="material" type="radio" name="category" id="material" />
+                    <label htmlFor="material">Study Materials</label>
+                    <br />
+                    <div className='w3-center'> <button onClick={() => condition ? navigate("/Input2") : navigate("/Input")} className='w3-block w3-black w3-button'>Next</button></div>
+                   
+                </div>
+            </div>
+            </div>
         </div >
     );
 }
