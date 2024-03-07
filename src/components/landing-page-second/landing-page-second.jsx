a7ec00379e0ce75ffe06881c42ff1173636783f8
Tanish2207 <bhamare.tn@gmail.com>
2024-03-07 09:26:23 +0530

    tpo landing page

--- src/components/landing-page-second/landing-page-second.jsx
+++ src/components/landing-page-second/landing-page-second.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
-
+import './landing-page-second.css'
+import { Link } from 'react-router-dom'
 function Landing_page_second() {
-  return (
-    <div>
-        <div className="landing_page_second_cont">
-            <div className="landing-2-box">
-                <img src="/Students.png" alt="" />
-                <h4>Students</h4>
-                <p>A centralized platform for scheduling placement interviews. </p>
-                <button className="get-started-btn students">Get Started</button>
-            </div>
-            <div className="landing-2-box">
-                <h4>Recruiter</h4>
-                <img src="/Recruiter.png" alt="" />
-                <p>View upcoming interviews, and manage their calendar more effectively. </p>
-                <button className="get-started-btn tpo">Get Started</button>
-            </div>
-            <div className="landing-2-box">
-                <h4>College</h4>
-                <img src="/Students.png" alt="" />
-                <p>Easily track the status of each candidate</p>
+    return (
+        <div>
+            <div className="landing_page_second_cont">
+                <div className="landing-2-box mybg1">
+                    <div className="inner-landing">
+                        <img src="/Students.png" alt="" width="25%" />
+                        <h4>Students</h4>
+                        <p>A centralized platform for scheduling placement interviews. </p>
+                    </div>
+                    <a href="/studentr"><button className="get-started-btn students">Get Started</button></a>
+                </div>
+                
+                <div className="landing-2-box mybg3">
+                    <div className="inner-landing">
+                        <img src="/College.png" alt="" width="25%" />
+                        <h4 className="mmyypp">TPO</h4>
+                        <h6 className="myh6">Training & Placement Office</h6>
+                        <p>Easily track the status of each candidate</p>
+                    </div>
+                   <Link to="/colleger"> <button className="get-started-btn tpo">Get Started</button></Link>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Landing_page_second
