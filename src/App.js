b18c22138b5f620a22eac3f2bbb439fb2b6d45b3
Ayaan <ayaanabdshaikh@gmail.com>
2024-03-07 07:33:34 +0530

    navdash addition

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import Navbar from './components/library/Navbar.jsx';
 import Home from './components/library/home.jsx';
 import Studentregistration from './components/login/registrationpage1.jsx';
 import data from './components/calendar/DataBase/data.jsx';
-import Home from './components/library/home';
+
 import Events from './components/calendar/Events/Events';
-import Studentregistration from './components/login/registrationpage1';
 import Experience from './components/library/inexpe';
-import Landing_page from './components/landing_page/landing_page';
+
 import Collegeregistration from './components/login/registrationpage3';
 import Companyregistration from './components/login/registrationpage2';
 import {  Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import { useEffect } from 'react';
-
+import navdash from './components/navbar_dash/navdash.jsx';
+import Navdash from './components/navbar_dash/navdash.jsx';
 
 
 function App() {
@@ -40,8 +40,10 @@ function App() {
          <Route path="/" element= {<Landing_page/>} />
          <Route path="/exp" element= {<Experience/>} />
          <Route path="/libhome" element= {<Home/>} />
+         <Route path="/navd" element= {<Navdash/>}/>
       </Routes>
      </BrowserRouter>
+     
       
 
     </div>
